35d30a5352967d3fd62839633cc3d3ba01656ca4
Sarah Witt <witt.s@husky.neu.edu>
2019-10-13 19:46:41 -0400

    update script and list bad smiles

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,11 @@
 from app import app
 from flask import render_template
+import scripts.pains.LillyMedchemRules.pains as pains
 
 @app.route('/')
 @app.route('/index')
 def index():
-  return render_template('index.html', title='Cheminformatics')
+  pains.run_pains_filter()
+  bad_smiles = pains.get_bad_smiles_and_reasons()
+
+  return render_template('index.html', title='Cheminformatics', smiles=bad_smiles)
