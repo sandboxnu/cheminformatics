7b2016d8ac30056d2b851293767a10b5cb90ad29
Sarah Witt <witt.s@husky.neu.edu>
2019-10-14 22:34:30 -0400

    Add input parameter to script

--- scripts/pains/LillyMedchemRules/pains.py
+++ scripts/pains/LillyMedchemRules/pains.py
@@ -4,21 +4,10 @@ import os, sys
 
 os.chdir(os.path.abspath(os.path.dirname(__file__)))
 
-inputs = ["COC1=CC2=C(C=C1OC)C(C1=CC=CC=C1)N(CC(=O)NS(=O)(=O)C1=CC=C(CN(C)CCC3=CC=CC=C3)C=C1)CC2",
-"COC1=CC(=CC(=C1)C(=O)NS(=O)(=O)C1=CC=C(CN(C)CCC2=CC=CC=C2)C=C1)OC",
-"CN(CCC1=CC=CC=C1)CC1=CC=C(S(=O)(=O)NC(=O)CCCN2CC(=C(C3=CC=C(F)C=C3F)C2=O)C2=CC=C(F)C=C2F)C=C1",
-"CCOC1=CC=C(C2=CC=C3C(=C2)C(=CN3C)C(=O)NS(=O)(=O)C2=CC=C(CN(C)CCC3=CC=CC=C3)C=C2)C=C1F",
-"CN(CCC1=CC=CC=C1)CC1=CC=C(S(=O)(=O)NC(=O)C2=CC=C(C3=CC=CC=N3)C=C2)C=C1",
-"CN(CCC1=CC=CC=C1)CC1=CC=C(S(=O)(=O)NC(=O)C2=CC=C3C(=C2)C=CN3CC2=CC=C(C(F)(F)F)C=C2C(F)(F)F)C=C1",
-"CN(CCC1=CC=CC=C1)CC1=CC=C(S(=O)(=O)NC(=O)C2=CC=C3OC(=CC3=C2)C2=CC=CC=C2)C=C1",
-"CN(CCC1=CC=CC=C1)CC1=CC=C(S(=O)(=O)NC(=O)C2=CC=C(CN3C(=O)C(=C(C4=CC=C(F)C=C4F)C3=O)C3=CC=C(F)C=C3F)C=C2)C=C1",
-"CN(CCC1=CC=CC=C1)CC1=CC=C(S(=O)(=O)NC(=O)C2=CC=CC(=C2)NCCC2=CC=CC=C2)C=C1",
-"CN(CCC1=CC=CC=C1)CC1=CC=C(S(=O)(=O)NC(=O)C2=CC=C3C(=C2)C(=CN3C)C(=O)C2=CC=CS2)C=C1"]
-
 smiles = []
 bad_smiles_and_reasons = {}
 
-def run_pains_filter():
+def run_pains_filter(inputs):
   pains = os.system('ruby Lilly_Medchem_Rules.rb input.smi > okmedchem.smi')
 
   bad_files = list(filter(os.path.isfile, ['bad0.smi', 'bad1.smi', 'bad2.smi', 'bad3.smi']))
@@ -44,11 +33,14 @@ def run_pains_filter():
   for smile in good_smiles:
     smiles.append(smile)
 
-def get_smiles():
+def get_smiles(inputs):
+  run_pains_filter(inputs)
   return smiles
 
-def get_bad_smiles():
+def get_bad_smiles(inputs):
+  run_pains_filter(inputs)
   return bad_smiles_and_reasons
 
-def get_bad_smiles_and_reasons():
+def get_bad_smiles_and_reasons(inputs):
+  run_pains_filter(inputs)
   return [k + v for k, v in bad_smiles_and_reasons.items()]
