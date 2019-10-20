964ad836bd7806d356025b19a5d182aa456891e4
aislinblack <1of4black@gmail.com>
2019-10-20 13:19:14 -0400

    examples smiles

--- smiles.py
+++ smiles.py
@@ -1,21 +1,9 @@
-smiles = [{'smile': "Fake Smile"}, {}]
+##example smile
+smile = dict([('smile', ''), ('murcko-smile', '')])
 
-##or
-class Smile(dict):
-    def __getattr__(self, name):
-        if name in self:
-            return self[name]
-        else:
-            raise AttributeError("No such attribute: " + name)
-
-    def __setattr__(self, name, value):
-        self[name] = value
-
-    def __delattr__(self, name):
-        if name in self:
-            del self[name]
-        else:
-            raise AttributeError("No such attribute: " + name)
-
-##
-smile = dict([('smile', 'jeje'), ('murcko-smile', 'j')])
+##example lists of smiles
+smiles = [smile]
+#add keys to smiles in the list
+for(s : smile){
+    s['murcko'] = "whatever"
+}
