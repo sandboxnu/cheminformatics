f1e5ff70e77072daabf282626bb32fbc7fd58971
Sarah Witt <witt.s@husky.neu.edu>
2019-10-21 10:01:19 -0400

    Use new smile dictionary and add example smiles

--- smiles.py
+++
@@ -1,9 +0,0 @@
-##example smile
-smile = dict([('smile', ''), ('murcko-smile', '')])
-
-##example lists of smiles
-smiles = [smile]
-#add keys to smiles in the list
-for(s : smile){
-    s['murcko'] = "whatever"
-}
