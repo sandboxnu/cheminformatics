2cca00131da0d6ab37035808df369c2ba64b3838
aislinblack <1of4black@gmail.com>
2019-10-24 19:19:30 -0400

    Adding in the code for two smiles, unsure how to proceed with all of the smiles and how to store than information

--- scripts/clustering/clustering.py
+++ scripts/clustering/clustering.py
@@ -1,10 +1,26 @@
-import chem from rdkit
+from rdkit import DataStructs
+from rdkit.Chem.Fingerprints import FingerprintMols
 #dictionary of dictionaries, each key is the smile
 
 #add a key with the tanimoto coefficient
 def add_tanimoto_coefficients(smiles):
     #figure out how to get rdkit in here
+    
     for smile in smiles:
+        #this won't work
         smiles[smile]['tanimoto'] = 0 #some number here
 
     return smiles
+
+#need to get the comparison for each smile it would seem, what is the best way to store
+#THAT information
+#smile 1 and smile two, are they murcko smiles? or 
+def compare_two_smiles(smile1, smile2):
+    smile1Ms = Chem.MolFromSmiles(smile1['murcko'])
+    smile2Ms = Chem.MolFromSmiles(smile2['murcko'])
+    fps1 = FingerprintMols.FingerprintMol(smile1Ms)
+    fps2 = FingerprintMols.FingerprintMol(smile2Ms)
+
+    DataStructs.FingerprintSimilarity(fps1, fps2)
+    
+
