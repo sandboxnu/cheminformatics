a3bbdc3a97d04173b8bb2eb03d546ff7e26a2537
Sarah Witt <witt.s@husky.neu.edu>
2019-10-31 10:28:44 -0400

    Simple cytoscape graph

---
+++ app/static/js/cluster.js
@@ -0,0 +1,57 @@
+const data = {"nodes": [{"data": {"id": '1', "type": "ellipse"}}]};
+
+let options = {
+  name: 'circle',
+
+  fit: true, 
+  padding: 30,
+  boundingBox: undefined, 
+  avoidOverlap: true, 
+  nodeDimensionsIncludeLabels: false,
+  radius: undefined, 
+  startAngle: 3 / 2 * Math.PI, 
+  sweep: undefined,
+  clockwise: true, 
+  sort: undefined, 
+  animationEasing: undefined, 
+  animateFilter: function ( node, i ){ return true; }, 
+  ready: undefined,
+  stop: undefined, 
+  transform: function (node, position ){ return position; } 
+
+};
+
+var cy = window.cy = cytoscape({
+  container: document.getElementById('cy'),
+
+  boxSelectionEnabled: false,
+  autounselectify: true,
+
+  style: [
+    {
+      selector: 'node',
+      style: {
+        'height': 20,
+        'width': 20,
+        'label': 'data(id)',
+      }
+    },
+
+    {
+      selector: 'edge',
+      style: {
+        'curve-style': 'haystack',
+        'haystack-radius': 0,
+        'width': 5,
+        'opacity': 0.5,
+        'line-color': '#f2f08c'
+      }
+    }
+  ],
+
+  elements: data
+});
+
+var layout = cy.layout( options );
+
+layout.run();
