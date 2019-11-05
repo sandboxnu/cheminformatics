1b3778c2537249ecc3e9a65a234f099fe5580b60
Sarah Witt <witt.s@husky.neu.edu>
2019-11-05 16:57:09 -0500

    Display simiarities and PAINs results on clustering page

--- app/routes.py
+++ app/routes.py
@@ -18,10 +18,12 @@ def cluster():
   inputs = smiles.keys()
   
   bad_smiles = pains.get_bad_smiles_and_reasons(inputs)
-  
+
   tanimoto_smiles = clustering.add_tanimoto_coefficients(smiles)
+  
+  tanimoto_smile_strings = [" ".join([str(i) for i in v['similarities'].values()]) for v in tanimoto_smiles.values()]
 
   get_smiles_json(tanimoto_smiles)
 
 
-  return render_template('cluster.html', title='Cheminformatic Analysis')
+  return render_template('cluster.html', title='Cheminformatic Analysis', bad_smiles=bad_smiles, tanimoto_smiles=tanimoto_smile_strings)
