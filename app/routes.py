2ee01446b9c3f175fecd5514c072d238e69932a6
aislinblack <1of4black@gmail.com>
2019-11-08 20:53:37 -0500

    connect html form for coefficient to get input

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,12 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 import scripts.pains.LillyMedchemRules.pains as pains
 from app.data.smiles import smiles
 from app.data.smiles import smiles_with_murcko
 import scripts.clustering.clustering as clustering
 from app.data.clustering import get_smiles_json
 
-global bad_smiles
+global bad_smiles 
 
 @app.route('/')
 @app.route('/index')
@@ -48,3 +48,21 @@ def verify_pains():
   global bad_smiles
 
   return render_template('pains_verify_and_coefficient_use.html', title='Cheminformatic Analysis', bad_smiles=bad_smiles)
+
+@app.route('/final_compounds', methods=['GET', 'POST'])
+def final_compounds():
+  if request.method == 'POST':
+    tanimoto = request.form.get('tanimoto'))
+  
+  #get real final smiles
+  inputs = smiles.keys()
+  global bad_smiles
+  bad_smiles={}
+
+  tanimoto_smiles = clustering.add_tanimoto_coefficients(smiles)
+  
+  tanimoto_smile_strings = [" ".join([str(i) for i in v['similarities'].values()]) for v in tanimoto_smiles.values()]
+
+  get_smiles_json(tanimoto_smiles)
+
+  return render_template('cluster.html', title='Cheminformatic Analysis', bad_smiles=bad_smiles, tanimoto_smiles=tanimoto_smile_strings)
