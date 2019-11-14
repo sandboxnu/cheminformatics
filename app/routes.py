96ae63a024efbef81c012f0b68ee5d12f56b61d7
aislinblack <1of4black@gmail.com>
2019-11-13 20:07:48 -0500

    functionality to the pains filtering

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,13 @@
 from app import app
 from flask import render_template, request
 import scripts.pains.LillyMedchemRules.pains as pains
-from app.data.smiles import smiles, construct_smiles, filter_smiles
+from app.data.smiles import smiles, construct_smiles, filter_smiles, convert
 import scripts.clustering.clustering as clustering
 from app.data.clustering import get_smiles_json
 
 global bad_smiles 
 global good_smiles
+global all_smiles
 
 @app.route('/')
 @app.route('/index')
@@ -24,10 +25,12 @@ def upload():
             return render_template('index.html', title='Cheminformatic Analysis', errors=["Please input a valid file format"])
 
         inputs = smiles.keys()
-        
+        global good_smiles
         good_smiles = filter_smiles(pains.get_smiles(inputs))
         global bad_smiles
         bad_smiles = pains.get_bad_smiles(inputs)
+        global all_smiles 
+        all_smiles = smiles
         
     return render_template('pains_verify_and_coefficient_use.html', title='Cheminformatic Analysis', bad_smiles=bad_smiles)
 
@@ -47,6 +50,7 @@ def pains_verify_and_coefficient_use():
 def verify_pains():
   global good_smiles
   global bad_smiles
+  global all_smiles
   if request.method == 'POST':
     form = request.form
     if form['action'] == 'Drop Selected Compounds':
@@ -59,7 +63,7 @@ def verify_pains():
       for smile in form:
         if(smile != 'action'):
           del bad_smiles[smile]
-          good_smiles[smile] = {}
+          good_smiles[smile] = all_smiles[smile]
 
   return render_template('pains_verify_and_coefficient_use.html', title='Cheminformatic Analysis', bad_smiles=bad_smiles)
 
@@ -70,13 +74,14 @@ def final_compounds():
   
   #get real final smiles
   inputs = smiles.keys()
+  global good_smiles
   global bad_smiles
   bad_smiles={}
 
-  tanimoto_smiles = clustering.add_tanimoto_coefficients(smiles)
+  tanimoto_smiles = clustering.add_tanimoto_coefficients(good_smiles)
   
   tanimoto_smile_strings = [" ".join([str(i) for i in v['similarities'].values()]) for v in tanimoto_smiles.values()]
 
-  get_smiles_json(tanimoto_smiles, .2)
+  get_smiles_json(tanimoto_smiles, float(tanimoto))
 
   return render_template('cluster.html', title='Cheminformatic Analysis', bad_smiles=bad_smiles, tanimoto_smiles=tanimoto_smile_strings)
