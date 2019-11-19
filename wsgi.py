231facf0161c4165effe88dd952820be9c0e823c
Sarah Witt <witt.s@husky.neu.edu>
2019-11-19 13:55:59 -0500

    Begin work on file reload

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,20 @@
 from app import app
 
+from os import path, walk
+
+
+
+
 if __name__ == '__main__':
+    extra_dirs = ['directory/to/watch',]
+    extra_files = extra_dirs[:]
+    for extra_dir in extra_dirs:
+        for dirname, dirs, files in walk(extra_dir):
+            for filename in files:
+                filename = path.join(dirname, filename)
+                if path.isfile(filename):
+                    extra_files.append(filename)
+
+    app.run(extra_files=extra_files)
     app.run()
 
