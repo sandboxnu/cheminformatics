2a4bbcae48fdbef6cdb69156870bead7f9301a89
Karmen <lu.kar@husky.neu.edu>
2020-01-26 17:58:41 -0500

    Paste code to add tooltips on hover.

--- app/static/js/cluster.js
+++ app/static/js/cluster.js
@@ -89,6 +89,35 @@ fetch('js/data.json', {mode: 'no-cors'})
     elements: data
   });
 
+
+  function makePopper(ele) {
+    let ref = ele.popperRef(); // used only for positioning
+
+    ele.tippy = tippy(ref, {
+      // tippy options:
+      content: () => {
+        let content = document.createElement("div");
+
+        content.innerHTML = ele.id();
+
+        return content;
+      },
+      trigger: "manual" // probably want manual mode
+    });
+  }
+
+  cy.ready(function() {
+    cy.elements().forEach(function(ele) {
+      makePopper(ele);
+    });
+  });
+
+  cy.elements().unbind("mouseover");
+  cy.elements().bind("mouseover", event => event.target.tippy.show());
+
+  cy.elements().unbind("mouseout");
+  cy.elements().bind("mouseout", event => event.target.tippy.hide());
+
   var layout = cy.layout( options );
 
   layout.run();
