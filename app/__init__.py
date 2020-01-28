e15799c5f737e7d1fbdfaae439f571d2480fe16f
aislinblack <1of4black@gmail.com>
2020-01-27 19:19:33 -0500

    Configure app to use session as a filesystem -move away from global variables

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,10 @@ import flask_excel as excel
 
 app = Flask(__name__, static_url_path='', static_folder='static')
 
+app.secret_key = 'some secret key' 
+app.config.update(
+    SESSION_TYPE = 'filesystem'
+)
 bootstrap = Bootstrap()
 bootstrap.init_app(app)
 
