6e139ad3dd38a9d771ba162c355bd67d99f5e617
Aislin Black <1of4black@gmail.com>
2020-09-29 10:33:11 -0400

    Update Morgan Finger Print radius (#130)
    * Update Morgan Finger Print radius
    
    * Remove fingerprint mols

--- scripts/clustering/clustering.py
+++ scripts/clustering/clustering.py
@@ -7,7 +7,7 @@ from app.data.smiles import  convert
 from rdkit.ML.Cluster import Butina
 from rdkit.Chem.AtomPairs import Pairs
 
-radius = 4
+radius = 2
 def compare_two_smiles(smile1, smile2, fp_type):
     smile1Ms = Chem.MolFromSmiles(smile1)
     smile2Ms = Chem.MolFromSmiles(smile2)
@@ -15,21 +15,18 @@ def compare_two_smiles(smile1, smile2, fp_type):
     if fp_type == "atom-pair":
         fps1 = Pairs.GetAtomPairFingerprintAsBitVect(smile1Ms)
         fps2 = Pairs.GetAtomPairFingerprintAsBitVect(smile2Ms)
-    elif fp_type == "fpm":
-        fps1 = FingerprintMols.FingerprintMol(smile1Ms)
-        fps2 = FingerprintMols.FingerprintMol(smile2Ms)
     else:
         fps1 = AllChem.GetMorganFingerprintAsBitVect(smile1Ms, radius, nBits=1024)
         fps2 = AllChem.GetMorganFingerprintAsBitVect(smile2Ms, radius, nBits=1024)
 
     return DataStructs.TanimotoSimilarity(fps1, fps2)
-    
+
 #return cluster of smile_keys
 def cluster(smile_keys, fp_type, cutoff=0.15):
     #note: it seems cutoff is one - similarity coefficient, it's euclidean distance I think??
     nfps = len(smile_keys)
     dists = []
-    
+
     data = [None] * nfps
     for i in range(0, nfps):
         murcko = convert(smile_keys[i])
@@ -41,7 +38,7 @@ def cluster(smile_keys, fp_type, cutoff=0.15):
         else:
             fps = AllChem.GetMorganFingerprintAsBitVect(mol, radius, nBits=1024)
         data[i] = fps
-    
+
 
     for i in range(1,nfps):
         sims = DataStructs.BulkTanimotoSimilarity(data[i],data[:i])
@@ -56,14 +53,14 @@ def cluster(smile_keys, fp_type, cutoff=0.15):
             corresponding_smile = smile_keys[element]
             set.append(corresponding_smile)
         clusters.append(set)
-    return clusters   
+    return clusters
 
 def in_same_cluster(s1, s2, clusters):
     for clust in clusters:
         if s1 in clust and s2 in clust:
             return True
-        
-    return False            
+
+    return False
 
 def get_tanimoto_coeffient_by_cluster(smiles, clusters, fp_type):
     for clust in clusters:
@@ -79,13 +76,13 @@ def get_tanimoto_coeffient_by_cluster(smiles, clusters, fp_type):
             if not 'isReclustered' in smiles[smile]:
                 smiles[smile]['isReclustered'] =  False # to apply to every smile
             index += 1
-    return smiles        
+    return smiles
 
 def recluster_singletons(smiles, clusters, recluster_coefficient, fp_type):
     singletons = []
     realClusters = []
     realSingletons = []
-    for clust in clusters: 
+    for clust in clusters:
         if(len(clust) == 1):
             singletons.append(clust[0])
         else:
@@ -97,10 +94,10 @@ def recluster_singletons(smiles, clusters, recluster_coefficient, fp_type):
     for singleton in singletons:
         centroid_similarities = []
         for cluster in realClusters:
-            centroid = cluster[0] 
+            centroid = cluster[0]
             similarity = compare_two_smiles(singleton, centroid, fp_type)
             centroid_similarities.append(similarity)
-        
+
         max_sim_index = max(centroid_similarities)
         if max_sim_index < 0:
             continue
@@ -111,10 +108,10 @@ def recluster_singletons(smiles, clusters, recluster_coefficient, fp_type):
             realClusters[max_sim_index].append(singleton)
         else:
             realSingletons.append(singleton)
-    
+
     for singleton in realSingletons:
         realClusters.append([singleton])
-            
+
     return [smiles, realClusters]
 
 def max(array):
@@ -124,6 +121,6 @@ def max(array):
         index = index + 1
         if x > currMax:
             currMax = x
-    return index        
-    
+    return index
+
 
